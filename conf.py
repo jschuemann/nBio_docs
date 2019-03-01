15d682f0fde306db2e3bf3ef1332e994759783d6
jschuemann <jschuemann@partners.org>
2019-03-01 15:19:52 -0500

    adding a Logo

--- conf.py
+++ conf.py
@@ -110,6 +110,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_logo = 'images/nBio.png'
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
